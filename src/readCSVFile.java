3993bc7705a4e2ff6eaa233734dea81e5728b6ce
VuPham93 <62596412+VuPham93@users.noreply.github.com>
2020-05-17 15:42:45 +0700

    Fix close bufferReader

--- src/readCSVFile.java
+++ src/readCSVFile.java
@@ -5,9 +5,7 @@ public class readCSVFile {
         File file = new File("country.csv");
         String line = "";
 
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(file));
-
+        try (BufferedReader reader = new BufferedReader(new FileReader(file));) {
             while ((line = reader.readLine()) != null) {
                 String[] country = line.split(",");
                 System.out.println("Country [ip range: " + country[1] + " - " + country[2] + ", IPv4 = " + country[3] + ", code= " + country[4] + " , name=" + country[5] + "]");
